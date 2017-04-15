e29aaa31af7d75827f0d02e41c0b67b8feb1873f
Sukankana <sukankana.chakraborty@gmail.com>
2017-04-15 09:43:20 +0100

    Update RFScript.R

--- RFScript.R
+++ RFScript.R
@@ -1,6 +1,8 @@
 library (party)
 library (randomForest)
 
+
+#Random Forest
 inputData <- read.csv("C:/Users/Sukankana/Documents/DataSets/cleaned.csv",header = T)
 input.roughfix <- na.roughfix(inputData)
 input.roughfix$churn <- as.character(input.roughfix$churn)
@@ -9,6 +11,8 @@ rf <- randomForest(churn ~pre_hnd_price+hnd_price+asl_flag+hnd_webcap+models+pho
 print(rf)
 getTree(rf)
 
+
+#Random Forest using CForestMod
 set.seed (100)
 train <- sample(1:nrow(input.roughfix), 0.7*nrow(input.roughfix))
 trainData <- input.roughfix[train,]
@@ -19,7 +23,7 @@ predicted <- predict(cForestMod, newdata = testData)
 table (true = actuals, pred = predict(cForestMod, newdata = testData))
 mean (testData$churn != predicted)
 
-
+#Decision Tree
 cTreeMod <- ctree (churn ~ pre_hnd_price + hnd_price + asl_flag +      hnd_webcap + models + phones, data = trainData)
 predicted <- predict(cTreeMod, newdata = testData)
 mean (testData$churn != predicted)
